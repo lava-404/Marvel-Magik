da5605eec96d6a2c038d93d9fe664007b8e6f0c6
laoneya <lavanyareddy10435@gmail.com>
2025-07-07 14:47:46 +0530

    domain added

--- services/firebase.js
+++ services/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyButIshBJXqHQhwgK0sUOXdG2xb9PINWIw",
-  authDomain: "marvel-magik.firebaseapp.com",
-  projectId: "marvel-magik",
-  storageBucket: "marvel-magik.firebasestorage.app",
-  messagingSenderId: "979138392456",
-  appId: "1:979138392456:web:02fddb00956bc8f91e4a3d",
-  measurementId: "G-LN2TD819CF"
+  apiKey: "AIzaSyA5Rdkohq_3jlVlIzLJqFzz9Edb2V4SkXA",
+  authDomain: "marvel-magik-fb269.firebaseapp.com",
+  projectId: "marvel-magik-fb269",
+  storageBucket: "marvel-magik-fb269.firebasestorage.app",
+  messagingSenderId: "481438669093",
+  appId: "1:481438669093:web:5a8776c0078fe3475222cc",
+  measurementId: "G-Q5KWHF3VTT"
 };
 
 const app = initializeApp(firebaseConfig);
